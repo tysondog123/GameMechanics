e657669119523c4c042743db572ead69825054bd
Taylor Raine <92051840+tysondog123@users.noreply.github.com>
2026-04-23 17:30:19 +0100

    Finish Tutorial

--- Uni_Platforming_Test/Intermediate/Build/Win64/x64/Nemesis/Development/Engine/SharedPCH.Engine.Cpp20.cpp
+++ Uni_Platforming_Test/Intermediate/Build/Win64/x64/Nemesis/Development/Engine/SharedPCH.Engine.Cpp20.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.43.34809
+// Compiler: 14.38.33145
 #include "SharedPCH.Engine.Cpp20.h"
--- Uni_Platforming_Test/Intermediate/Build/Win64/x64/Nemesis/Development/Slate/SharedDefinitions.Slate.Cpp20.h
+++ Uni_Platforming_Test/Intermediate/Build/Win64/x64/Nemesis/Development/Slate/SharedDefinitions.Slate.Cpp20.h
@@ -60,7 +60,7 @@
 #define UBT_COMPILED_TARGET Game
 #define UE_APP_NAME "UnrealGame"
 #define UE_WARNINGS_AS_ERRORS 0
-#define UE_ENGINE_DIRECTORY "../../../../../../../../Game Projects/UE_5.6/Engine/"
+#define UE_ENGINE_DIRECTORY "../../../../../../../../Program Files/Epic Games/UE_5.6/Engine/"
 #define FORCE_ANSI_ALLOCATOR 0
 #define USE_MALLOC_BINNED2 1
 #define USE_MALLOC_BINNED3 0
